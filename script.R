b9f87abbae0699f35af96548ce779ecc9be8cb9d
matteofloris <matteo.floris@gmail.com>
2020-03-18 19:06:08 +0100

    Update script.R
    added function to calculate new daily deaths

--- script.R
+++ script.R
@@ -14,6 +14,17 @@ for(file in files) {
   #dat <- cbind(dat, pre) 
 } 
 
-ggplot(dat, aes(x = as.factor(dat$data), y = dat$nuovi_attualmente_positivi)) + geom_point() + facet_wrap( ~ dat$denominazione_regione)+ theme(legend.position = "none")+theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
+### calculate new daily deaths 
+df <- data.frame()
+for (region in dat$denominazione_regione)
+{
+s <- subset(dat, dat$denominazione_regione %in% c(region))
+s$diff_decessi <- c(0, diff(s$deceduti, lag = 1, differences = 1))
+df <- rbind(df, s)
+}
+
+ggplot(df, aes(x = as.factor(df$data), y = df$diff_decessi)) + geom_point() + facet_wrap( ~ df$denominazione_regione)+ theme(legend.position = "none")+theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
+
+
 
 
